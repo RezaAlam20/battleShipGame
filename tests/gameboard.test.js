b492590332e3c1ab3405da062d163611875ed284
RezaAlam20 <rzaalam384@gmail.com>
2025-12-22 15:25:19 +0330

    take hits, sinking , tracking missed , ship occupied squares , ship placement tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -11,7 +11,37 @@ test("place Ship", () => {
 });
 
 test("take attacks", () => {
-  testBoard.placeShip(1, 6, new Ship(5));
+  testBoard.placeShip(1, 6, new Ship(2));
   expect(testBoard.receiveAttack(1, 6)).toBe(true);
-  expect(testBoard.receiveAttack(10, 2)).toBe(false);
+  expect(testBoard.receiveAttack(9, 2)).toBe(false);
+});
+test("ship hit and sink", () => {
+  expect(testBoard.showShip(1, 6).hitcount).toBe(1);
+  testBoard.receiveAttack(1, 6);
+  expect(testBoard.showShip(1, 6).isSunk()).toBe(true);
+});
+
+test("track missed", () => {
+  testBoard.receiveAttack(9, 9);
+  expect(testBoard.showShip(9, 9)).toBe(null);
+});
+
+test("check all ships", () => {
+  expect(testBoard.allSunk()).toBe(false);
+  testBoard.receiveAttack(5, 4);
+  testBoard.receiveAttack(5, 4);
+  expect(testBoard.allSunk()).toBe(true);
+});
+
+test("ship occupied squares", () => {
+  expect(testBoard.showShip(1, 6)).toBeTruthy();
+  expect(testBoard.showShip(1, 7)).toBeTruthy(); // ships length
+  testBoard.placeShip(4, 8, new Ship(3), "vertical");
+  expect(testBoard.showShip(5, 8)).toBeTruthy();
+  expect(testBoard.showShip(6, 8)).toBeTruthy();
+});
+
+test("place ship outside the board", () => {
+  testBoard.placeShip(6, 10, new Ship(5));
+  expect(testBoard.showShip(6, 10)).toBeFalsy();
 });
