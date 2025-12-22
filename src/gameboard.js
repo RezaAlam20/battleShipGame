7f12326d17ca9bb9f49e691bf13eee10e0a260f8
RezaAlam20 <rzaalam384@gmail.com>
2025-12-22 15:24:32 +0330

    expanded placeships and added allSunk function lines 8/66

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,18 +5,65 @@ class Gameboard {
     this.size = 10;
     this.board = [[], [], [], [], [], [], [], [], [], []];
   }
-  placeShip(x, y, ship) {
-    if (this.board[x][y] == undefined) {
-      this.board[x][y] = ship;
-      return true;
+  placeShip(x, y, ship, mode = "horizontal") {
+    if (mode == "horizontal") {
+      if (y + ship.length > 9) {
+        return false;
+      }
+      if (
+        this.board[x][y] == undefined &&
+        this.board[x][y + ship.length] == undefined // might cause bugs not cheking the middle
+      ) {
+        for (let i = 0; i < ship.length; i++) {
+          this.board[x][y + i] = ship;
+        }
+        return true;
+      }
+    } else if (mode == "vertical") {
+      if (x + ship.length > 9) {
+        return false;
+      }
+      if (
+        this.board[x][y] == undefined &&
+        this.board[x + ship.length][y] == undefined
+      ) {
+        for (let i = 0; i < ship.length; i++) {
+          this.board[x + i][y] = ship;
+        }
+        return true;
+      }
     }
+
     return false;
   }
   receiveAttack(x, y) {
     if (this.board[x][y] != undefined) {
+      this.board[x][y].hit();
       return true;
+    } else {
+      this.board[x][y] = null;
+      return false;
     }
-    return false;
+  }
+  showShip(x, y) {
+    return this.board[x][y];
+  }
+  allSunk() {
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board.length; j++) {
+        if (this.board[i][j] == undefined || this.board[i][j] == null) {
+          continue;
+        } else {
+          let state = this.board[i][j].isSunk();
+          if (state) {
+            continue;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 }
 
