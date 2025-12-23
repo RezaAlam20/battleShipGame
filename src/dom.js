dbcc6eb49aad8245e36f0853c503f771d15d5577
RezaAlam20 <rzaalam384@gmail.com>
2025-12-23 14:34:53 +0330

    added dom elements and reset dom function lines 18-46

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,6 @@
 export function dom() {}
-
+let content = document.querySelector(".content");
+let nav = document.querySelector("nav");
 let firstGrid = document.querySelector(".firstGrid");
 let secondGrid = document.querySelector(".secondGrid");
 createGrid(firstGrid);
@@ -14,3 +15,32 @@ function createGrid(grid) {
     }
   }
 }
+
+let startBtn = document.createElement("btn");
+startBtn.classList.add("startBtn");
+startBtn.textContent = "Start Game";
+
+let firstWindow = document.createElement("div");
+let secondWindow = document.createElement("div");
+
+firstWindow.classList.add("window");
+secondWindow.classList.add("window");
+
+nav.appendChild(firstWindow);
+nav.appendChild(startBtn);
+nav.appendChild(secondWindow);
+
+let winner = document.querySelector(".winner");
+function resetDom() {
+  while (firstGrid.firstElementChild) {
+    firstGrid.removeChild(firstGrid.firstElementChild);
+  }
+  while (secondGrid.firstElementChild) {
+    secondGrid.removeChild(secondGrid.firstElementChild);
+  }
+
+  createGrid(firstGrid);
+  createGrid(secondGrid);
+}
+
+export { resetDom };
